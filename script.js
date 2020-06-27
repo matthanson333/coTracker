9e0830c627bf43e74e4b112a4b59c1013af213b6
matthanson333 <matthanson333@gmail.com>
2020-06-27 09:47:27 -0400

    added files

--- script.js
+++ script.js
@@ -1,21 +1,85 @@
-$(document).ready(function () {
-  $("#submitButton").on("click", function (event) {
+// $(document).ready(function () {
+//   $("#submitButton").on("click", function (event) {
+//     event.preventDefault();
+//     var inputVal = $("#cityInput").val();
+//     console.log(inputVal);
+//     //   Empty search field
+//     $("#cityInput").val("");
+//     citySearch(inputVal);
+//   });
+
+//   function citySearch(inputVal, ) {
+//       $.ajax({
+//           type: "GET",
+//           url:
+//       }).then(function (data) {
+
+//       })
+//   }
+// });
+
+// Summer's Code
+$(document).ready(() => {
+  //Variables
+  //SUBMIT BUTTON
+  /*Description: Button, that once pressed, provides information on city input into the search form 
+    to be displayed as current stats and reference information*/
+  $("#submitBtn").on("click", function (event) {
     event.preventDefault();
-    var inputVal = $("#cityInput").val();
-    console.log(inputVal);
-    //   Empty search field
-    $("#cityInput").val("");
-    citySearch(inputVal);
+    var state = $("#search-term").val();
+    console.log(state);
+    currentCOVIDStats(state);
+    stateResources(state);
   });
-
-  function citySearch(inputVal, ) {
-      $.ajax({
-          type: "GET",
-          url:
-      }).then(function (data) {
-
-      
-
-      })
+  //currentCOVIDStats FUNCTION
+  /*Description: Function collects current COVID data on state provided as input 
+    and diplays on page*/
+  function currentCOVIDStats(state) {
+    $.ajax({
+      method: "GET",
+      url: `https://covidtracking.com/api/v1/states/${state}/current.json`,
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+        //currentCOVIDStats Variables
+        var state = response.state;
+        var totalTested = response.totalTestsViral;
+        var totalConfirmedCases = response.positiveCasesViral;
+        var totalCurrentHospitalizations = response.hospitalizedCurrently;
+        var dailyChange = response.totalTestResultsIncrease;
+        $("#stateStatsDiv").addClass("." + state + " ");
+        $("totalTestedDiv").text(totalTested);
+        $("totalConfirmedCasesDiv").text(totalConfirmedCases);
+        $("#totalCurrentHospitalizationsDiv").text(
+          totalCurrentHospitalizations
+        );
+        $("#dailyChangeDiv").text(dailyChange);
+      },
+    });
+  }
+  //stateResources FUNCTION
+  /*Description: Function collects current COVID data on state provided as input 
+    and diplays on page*/
+  function stateResources(state) {
+    $.ajax({
+      method: "GET",
+      url: `https://covidtracking.com/api/v1/states/{state}/info.json`,
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+        //currentWeather Variables
+        var stateName = response.state;
+        var stateNotes = response.notes;
+        var stateCurrentCOVIDSite = response.covid19Site;
+        var stateOldCOVIDSite = response.covid19SiteOld;
+        var stateTwitter = response.twitter;
+        $("#stateName").html("<h1>" + stateName + "<h1>");
+        $("#stateNotes").html("<p>" + stateNotes + "<p>");
+        $("#stateCOVIDSiteLink")
+          .attr("href", stateCurrentCOVIDSite)
+          .text("State COVID Reference Site");
+        $("#stateTwitter").attr("href", stateTwitter).text(stateTwitter);
+      },
+    });
   }
 });
